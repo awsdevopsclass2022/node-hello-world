5a6099c4f5db542064578da500bd95845304388a
awsdevopsclass2022 <110197646+awsdevopsclass2022@users.noreply.github.com>
2023-08-17 21:19:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 const express = require('express')
 const app = express();
 
-const port = 8080;
+const port = 8086;
 const host = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello World from IBM Cloud Essentials!');
+  res.send('Hello World from IBM Cloud Essentials! Austin Class');
 })
 
 app.listen(port, host);
